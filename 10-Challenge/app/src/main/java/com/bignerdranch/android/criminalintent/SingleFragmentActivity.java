a2cdf2acb9a02ba739a8112ca31346127cc8c984
dymyll <dkjones1@buffs.wtamu.edu>
2018-05-06 14:38:05 -0500

    Uploading chapter 10

--- 10-Challenge/app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
+++ 10-Challenge/app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-
+//chapter10
 public abstract class SingleFragmentActivity extends AppCompatActivity {
     protected abstract Fragment createFragment();
 
